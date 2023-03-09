6d78596478e8d3b0b850dc9fad99ca89bbc0442f
Alberto <Alberto.Sanz5@alu.uclm.es>
2023-03-09 18:12:50 +0100

    upload first

---
+++ API/Jugador.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBA_Analyzer.API
+{
+    internal class Jugador
+    {
+        public Jugador() { 
+        }
+        public string id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set;}
+        public string position { get; set; }
+        public Int32 height_feet { get; set; }
+        public Int32 height_inches { get; set;}
+        public Int32 weigth_pounds { get;set;}
+        public Team equipo { get; set; }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using NBA_Analyzer.API;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace NBA_Analyzer
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static HttpClient client = new HttpClient();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void boton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                string url = "https://www.balldontlie.io/api/v1/teams";
+
+                client.DefaultRequestHeaders.Clear();
+
+                var response = client.GetAsync(url).Result;
+
+                var res = response.Content.ReadAsStringAsync().Result;
+                dynamic r = JObject.Parse(res);
+                foreach(var equipo in r.data)
+                {
+                    listaJugadores.Items.Add(equipo.full_name);
+                }
+                
+
+            }
+        }
+    }
+}
