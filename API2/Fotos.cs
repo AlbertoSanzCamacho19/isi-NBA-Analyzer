3b9cea43f9a577a78b2dd699942fc37f055b26bc
Alberto <Alberto.Sanz5@alu.uclm.es>
2023-03-11 17:30:22 +0100

    API de las imagenes funcional

---
+++ API2/Fotos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBA_Analyzer.API2
+{
+    internal class Fotos
+    {
+        public String url { get; set; }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,8 @@ using System.Net.Http.Headers;
 using NBA_Analyzer.API;
 using System.IO;
 using Newtonsoft.Json.Linq;
+using static System.Net.WebRequestMethods;
+using NBA_Analyzer.API2;
 
 namespace NBA_Analyzer
 {
@@ -32,7 +34,19 @@ namespace NBA_Analyzer
         {
             InitializeComponent();
 
-
+            using (var client = new HttpClient())
+            {
+                string url2 = "https://image-charts.com/chart.js/2.8.0?bkg=white&c=";
+                client.DefaultRequestHeaders.Clear();
+                string data = "[ 23, 64, 21, 53, -39, -30, 28, -10]";
+                string prametros = "{  \"type\": \"line\", \"data\": {   \"labels\": [\"Jan\", \"Feb\", \"Mar\", \"Apr\", \"May\", \"Jun\", \"Jul\", \"Aug\"],    \"datasets\": [      {        \"backgroundColor\": \"rgba(255,150,150,0.5)\",       \"borderColor\": \"rgb(255,150,150)\",       \"data\": "+data+",        \"label\": \"Dataset\",       \"fill\": \"origin\"      }    ]  }}";
+                dynamic jsonString = JObject.Parse(prametros);
+                Fotos fotos = new Fotos();
+                var httpContent = new StringContent(jsonString.ToString(), Encoding.UTF8);
+                fotos.url= url2 + "" + jsonString;
+                
+                DataContext = fotos;
+            }
 
         }
 
@@ -56,5 +70,24 @@ namespace NBA_Analyzer
 
             }
         }
+
+        private void boton2_Click(object sender, RoutedEventArgs e)
+        {
+            using (var client = new HttpClient())
+            {
+                string url2 = "https://image-charts.com/chart";
+                client.DefaultRequestHeaders.Clear();
+                string prametros = "{  \"type\": \"line\"  \"data\": {  \"labels\": [\"Jan\", \"Feb\", \"Mar\", \"Apr\", \"May\", \"Jun\", \"Jul\", \"Aug\"],  \"datasets\": [ { \"backgroundColor\": \"rgba(255,150,150,0.5)\",    \"borderColor\": \"rgb(255,150,150)\",   \"data\": [-23, 64, 21, 53, -39, -30, 28, -10], \"label\": \"Dataset\",        \"fill\": \"origin\" }    ]  }}";
+                dynamic jsonString=JObject.Parse(prametros);
+                Fotos fotos = new Fotos();
+                var httpContent=new StringContent(jsonString.ToString(),Encoding.UTF8);
+                fotos.url = "https://image-charts.com/chart.js/2.8.0?bkg=white&c={type:'line',data:{labels:['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug'],datasets:[{backgroundColor:'rgba(255,150,150,0.5)',borderColor:'rgb(255,150,150)',data:[-23,64,21,53,-39,-30,28,-10],label:'Dataset',fill:'origin'}]}}";
+          
+                DataContext = fotos;
+            }
+        }
+
+
+
     }
 }
